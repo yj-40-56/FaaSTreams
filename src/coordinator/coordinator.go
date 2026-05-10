4ed644a03bd05cb71ae5bece041a2003385882ba
mardentub <marc.dennhardt@campus.tu-berlin.de>
2026-05-10 22:26:09 +0200

    feat(coordinator): refactor coordinator to be FaaS-compatible

--- src/coordinator/coordinator.go
+++ src/coordinator/coordinator.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"log"
-	"strconv"
 	"time"
 
 	"coordinator/config"
@@ -13,97 +12,80 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const windowSize = 60 * time.Second
+
 type Event struct {
 	Timestamp time.Time
-	Raw       map[string]string
 }
 
 // TODO: Currently only supports tumbling windows, add support for other window types
-// Coordinator listens to Pub/Sub messages, extracts event data and stores it in Redis sorted set
 type Coordinator struct {
 	redisClient *redis.Client
-	windowSize  time.Duration
-	windowEnd   time.Time
 	queryConfig config.QueryConfig
 }
 
 func NewCoordinator(redisClient *redis.Client, queryConfig config.QueryConfig) *Coordinator {
-	windowSize := time.Duration(queryConfig.WindowSizeInSeconds) * time.Second
-
-	coordinator := &Coordinator{
+	return &Coordinator{
 		redisClient: redisClient,
-		windowSize:  windowSize,
 		queryConfig: queryConfig,
 	}
-
-	return coordinator
 }
 
-// Run listens to Pub/Sub messages, extracts event data and stores it in Redis sorted set
-func (c *Coordinator) Run(ctx context.Context, subscription *pubsub.Subscription) {
-	subscription.ReceiveSettings.MaxOutstandingMessages = 1
-	subscription.ReceiveSettings.NumGoroutines = 1
-	subscription.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
-		var data map[string]string
-		err := json.Unmarshal(msg.Data, &data)
-		if err != nil {
-			msg.Ack()
-			return
-		}
-
-		event := c.parseEventFromMap(data)
-		c.handleEvent(ctx, event, msg.Data)
-
+// Run is invoked by GCP when a Pub/Sub message arrives
+func (c *Coordinator) Run(ctx context.Context, msg *pubsub.Message) error {
+	var raw map[string]string
+	if err := json.Unmarshal(msg.Data, &raw); err != nil {
 		msg.Ack()
-	})
-}
-
-// Write sub data into Event struct
-func (c *Coordinator) parseEventFromMap(data map[string]string) *Event {
-	timestamp, err := time.Parse("02/01/2006 15:04:05", data["# Timestamp"])
-	if err != nil {
 		return nil
 	}
-
-	return &Event{
-		Timestamp: timestamp,
-		Raw:       data,
+	timestamp, err := time.Parse("02/01/2006 15:04:05", raw["# Timestamp"])
+	if err != nil {
+		msg.Ack()
+		return nil
 	}
+	event := &Event{Timestamp: timestamp}
+	if err := c.processIncomingEvent(ctx, event, msg.Data); err != nil {
+		return err
+	}
+	msg.Ack()
+	return nil
 }
 
-// Store event in Redis sorted set, where score is timestamp and member is JSON data, sorted by score
-func (c *Coordinator) handleEvent(ctx context.Context, event *Event, rawData []byte) {
-	if c.windowEnd.IsZero() {
-		c.windowEnd = event.Timestamp.Add(c.windowSize)
-		log.Printf("[Coordinator] First window ends at: %s\n", c.windowEnd.Format("15:04:05"))
-	}
-
-	score := float64(event.Timestamp.Unix())
-
-	// Store raw JSON directly in Redis
-	c.redisClient.ZAdd(ctx, "mod-stream", redis.Z{
-		Score:  score,
+// Store event in Redis sorted set and evaluate window boundary
+func (c *Coordinator) processIncomingEvent(ctx context.Context, event *Event, rawData []byte) error {
+	errRedis := c.redisClient.ZAdd(ctx, "mod-stream", redis.Z{
+		Score:  float64(event.Timestamp.Unix()),
 		Member: string(rawData),
-	})
+	},
+	).Err()
 
-	// Start worker for previous window
-	if event.Timestamp.After(c.windowEnd) {
-		windowStart := c.windowEnd.Add(-c.windowSize)
-		c.triggerWorker(ctx, windowStart, c.windowEnd)
-		c.windowEnd = c.windowEnd.Add(c.windowSize)
+	if errRedis != nil {
+		return errRedis
 	}
+
+	timeLeft, _ := c.redisClient.TTL(ctx, "window:active").Result()
+
+	if timeLeft == -2 {
+		c.redisClient.Set(ctx, "window:active", "1", 60*time.Second)
+	} else if timeLeft > 0 {
+
+	} else {
+		from := int64(0)
+		to, _ := c.redisClient.ZCard(ctx, "mod-stream").Result()
+		to = to - 1
+
+		for i := 0; i < len(c.queryConfig.SQLQueries); i++ {
+			query := c.queryConfig.SQLQueries[i]
+			c.triggerWorker(ctx, from, to, query)
+		}
+
+		c.redisClient.Set(ctx, "window:active", "1", 60*time.Second)
+	}
+	return nil
 }
 
+// TODO: Start actual worker, currently only logs
 // TODO: Pass window_start, window_end, query
-func (c *Coordinator) triggerWorker(ctx context.Context, windowStart time.Time, windowEnd time.Time) {
-	minScore := strconv.FormatInt(windowStart.Unix(), 10)
-	maxScore := strconv.FormatInt(windowEnd.Unix(), 10)
-
-	log.Printf("[Coordinator] Triggering worker for window (scores): %s - %s\n", minScore, maxScore)
-
-	for i := 0; i < len(c.queryConfig.SQLQueries); i++ {
-		query := c.queryConfig.SQLQueries[i]
-		log.Printf("[Coordnator] Triggering worker for query: %s\n", query.Name)
-		//TODO: spawn worker
-	}
+func (c *Coordinator) triggerWorker(ctx context.Context, from int64, to int64, query config.SQLQuery) {
+	log.Printf("[Coordinator] Triggering worker for query: %s, from: %d, to: %d\n", query.Name, from, to)
 }
